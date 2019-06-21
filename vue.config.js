7e54511d43915da7241e657bf7b3c6ef0bd4dd60
zzzcat013 <6405614+zzzcat013@users.noreply.github.com>
2019-06-21 22:54:13 +0800

    add devserver mock api

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const  appData= require('./data.json')
+const seller = appData.seller
+const goods= appData.goods
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -11,6 +15,26 @@ module.exports = {
     'cube-ui': {
       postCompile: false,
       theme: false
+    }
+  },
+  devServer:{
+    port: 8085, // 端口号
+    host: "localhost",
+    https: false, // https:{type:Boolean}
+    open: true, //配置自动启动浏览器
+    before(app){
+      app.get('/api/seller',function (req,res) {
+        res.json({
+          errno:0,
+          data: seller
+        })
+      })
+      app.get('/api/goods',function (req,res) {
+        res.json({
+          errno:0,
+          data: goods
+        })
+      })
     }
   }
 }
