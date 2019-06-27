80d042da0e24d4547bc00a19b3c1cccab5deaf4e
zzzcat013 <6405614+zzzcat013@users.noreply.github.com>
2019-06-27 14:48:09 +0800

    add cubeui-button

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <cube-button @click="showDialog">show dialog</cube-button>
   </div>
 </template>
+<script>
+  export default {
+    methods: {
+      showDialog() {
+        this.$createDialog({
+          type: 'alert',
+          title: '提示',
+          content: 'Cube-UI 已经成功安装！'
+        }).show()
+      }
+    }
+  }
+</script>
